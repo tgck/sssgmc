b236a3156b999c3a5f7b8f0f3e96f1f17f76ac71
tgck <taniguchiko@gmail.com>
2017-04-02 21:00:21 +0900

    test result up

---
+++ pre/sample/arduino/receive_serial_switch/receive_serial_switch.ino
@@ -0,0 +1,102 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+#define PIN 6
+#define PIN_DEBUG 13
+
+// Arduino Sample - Sereal Receive
+// シリアルから 文字 a を受けるたびに ボード上のLランプが光る
+char ASC_A = 'a';
+char ASC_B = 'b';
+char ASC_C = 'c';
+char ASC_Z = 'z';
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // リセット後にアップロードを許容するような マージンを持っておく
+  delay(1000);
+  
+  Serial.begin(9600);
+  pinMode(PIN_DEBUG, OUTPUT);
+
+  strip.begin();
+  strip.show();
+}
+
+void loop() { 
+  if (Serial.available() > 0) {
+    
+    blink(5);
+    char c = Serial.read();
+    
+    if (c == ASC_A) {
+
+      // switch pattern
+      colorWipe(strip.Color(255, 0, 0), 50);
+
+      // notificate
+      nodd();
+     
+    } else if (c == ASC_B) {
+
+      // switch pattern
+      colorWipe(strip.Color(0, 255, 0), 50);
+
+      nodd();
+      
+    } else if (c == ASC_C) {
+
+      // switch pattern
+      colorWipe(strip.Color(0, 0, 255), 50);
+      
+      nodd();
+      
+    } else if (c == ASC_Z) {
+      
+      // switch pattern
+      colorWipe(strip.Color(1, 1, 1), 50);
+
+      nodd();
+    }
+  }
+}
+
+// patterns 
+// from adafruit
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+
+// 光って消える
+void lightOnce(){
+    digitalWrite(PIN_DEBUG, HIGH);
+    delay(1300);
+    digitalWrite(PIN_DEBUG, LOW);
+    delay(1300);
+}
+
+void blink(int count){ // ...
+    for (int i=0; i<count; i++){
+      digitalWrite(PIN_DEBUG, HIGH);
+      delay(130);
+      digitalWrite(PIN_DEBUG, LOW);
+      delay(130);
+    }
+    delay(300); // space for next.
+}
+
+void nodd(){// -...
+      digitalWrite(PIN_DEBUG, HIGH);
+      delay(540);
+      digitalWrite(PIN_DEBUG, LOW);
+      delay(140);
+
+      blink(3);
+}
+
---
+++ pre/sample/konashi/konashi_003/index.js
@@ -0,0 +1,86 @@
+// forked from tvsk's "konashi_002" http://jsdo.it/tvsk/4BOu
+// forked from tvsk's "konashi_001" http://jsdo.it/tvsk/qlG9
+// UART Write
+
+////////////////////////////////////////////////////////////
+// Main
+////////////////////////////////////////////////////////////
+k.ready(function(){
+    
+    setupLed();
+    setupUart();
+    
+    ledOn();
+});
+
+k.find();
+
+
+////////////////////////////////////////////////////////////
+// Using UART 
+////////////////////////////////////////////////////////////
+function setupUart(){
+    console.log('setupUart');
+
+    k.uartBaudrate(k.KONASHI_UART_RATE_9K6);
+    k.uartMode(k.KONASHI_UART_ENABLE);
+    // k.uartWrite('cccaa');
+    // k.uartWrite(0x2e);
+    
+}
+
+function send(){
+    console.log('functinon send');
+    
+    var ascii = 'a';
+    k.uartWrite(ascii.charCodeAt(0)); // uartWrite の引数は数値
+}
+
+function sendAscii(char) {
+    console.log('functinon sendAscii');
+    k.uartWrite(char.charCodeAt(0)); // uartWrite の引数は数値
+}
+
+////////////////////////////////////////////////////////////
+// Simple notification on board for Debug!
+////////////////////////////////////////////////////////////
+function setupLed(){
+    k.pinMode(k.LED2, k.OUTPUT);   
+}
+function ledOn(){
+    k.digitalWrite(k.LED2, k.HIGH);    
+}
+function ledOff(){
+    k.digitalWrite(k.LED2, k.LOW);
+}
+
+////////////////////////////////////////////////////////////
+// Handlers
+////////////////////////////////////////////////////////////
+$(function(){
+
+    $(".info").text("initialize..");
+    
+    $(".tap-to-send").on('click', send);
+    $(".tap-to-send").on('click', ledOff);
+    
+    $(".tap-to-send-char").on('click', function(ev){
+        var ascii = $(ev.target).data('key');
+        var $info = $(".info");
+        $info.empty().text('sendAscii ' + ascii);
+
+        sendAscii(ascii);
+    });
+    $(".tap-to-send-char").on('click', ledOff);
+    
+    // イベント発生を通知するメッセージ
+    $(".tap").on('click', function(){
+        console.log('tapped!');
+        
+        var $info = $(".info");
+        $info.empty().text('tapped!');
+        setTimeout(function(){
+            $info.empty()
+        }, 1888);
+    });
+});

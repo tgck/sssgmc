654830cd7a1d109920a12dae7dc5073e4ec8a1a8
tgck <taniguchiko@gmail.com>
2017-05-02 22:38:20 +0900

    update (doesn't work)

---
+++ pre/sample/arduino/receive_serial_switch3/client.js
@@ -0,0 +1,142 @@
+// forked from tvsk's "konashi_003" http://jsdo.it/tvsk/y3Lz
+// forked from tvsk's "konashi_002" http://jsdo.it/tvsk/4BOu
+// forked from tvsk's "konashi_001" http://jsdo.it/tvsk/qlG9
+// UART Write
+
+////////////////////////////////////////////////////////////
+// Main
+////////////////////////////////////////////////////////////
+k.ready(function(){
+
+    setupLed();
+    setupUart();
+
+    ledOn();
+});
+
+k.find();
+
+
+////////////////////////////////////////////////////////////
+// Using UART
+////////////////////////////////////////////////////////////
+function setupUart(){
+    console.log('setupUart');
+
+    k.uartBaudrate(k.KONASHI_UART_RATE_9K6);
+    k.uartMode(k.KONASHI_UART_ENABLE);
+    // k.uartWrite('cccaa');
+    // k.uartWrite(0x2e);
+
+}
+
+function send(){
+    console.log('functinon send');
+
+    var ascii = 'a';
+    k.uartWrite(ascii.charCodeAt(0)); // uartWrite の引数は数値
+}
+
+// 1文字ずつ受け付ける
+// 'A' とか '9' とか 'F'  を渡す
+function convertAscii2ColorInt(char){
+//    if (char.length !== 1){ console.log("invalid arg"); return}
+    var result, hex;
+
+    var num = char.charCodeAt(0);
+    // var num = char; // とりあえず
+
+    if (num >= 48 && num <= 57) {
+        hex = (num - 48);
+    } // 0 -> 9
+    else if (num >= 65 && num <= 70 ) { // 大文字
+        hex = (num - 55);
+    } else {
+        return 0;
+    }
+    result = hex * 16;
+    console.log('char[' + char +']:' + result);
+    return result;
+}
+
+function sendAscii(str) {
+    if (str.length == 4) {
+        // debug1 -------------------------------------------------------->>
+        // k.uartWrite('b'.charCodeAt(0));// 単発で投げるのはうまくいく
+        // debug --------------------------------------------------------<<
+
+        // debug2 -------------------------------------------------------->>
+        k.uartWrite(0);
+        k.uartWrite(128);
+        k.uartWrite(64);
+        k.uartWrite(64);
+        // debug --------------------------------------------------------<<
+
+
+
+        console.log('function sendAscii 4chars ' + str.split(''));
+        // 固定長 4Byte想定
+        var arr = str.split('').map(function(v,i){
+            // console.log(i + ':' + v);
+            return convertAscii2ColorInt(v); // char 文字 を渡す
+        });
+        console.log(arr); // --> [0, 208, 64, 224] など
+        arr.forEach(function(v,i) {
+            // console.log('forEach:' + i + ':' + v);
+            k.uartWrite(v);
+        });
+        // 改行コードを送った方がいいのかも
+        //k.uartWrite(convertAscii2ColorInt(str.charCodeAt(0))); // uartWrite の引数は数値 '#' -> 数値0
+        //k.uartWrite(convertAscii2ColorInt(str.charCodeAt(1))); // uartWrite の引数は数値 R
+        //k.uartWrite(convertAscii2ColorInt(str.charCodeAt(2))); // uartWrite の引数は数値 G
+        //k.uartWrite(convertAscii2ColorInt(str.charCodeAt(3))); // uartWrite の引数は数値 B
+    } else if (str.length == 1) {
+        console.log('function sendAscii 1char');
+        k.uartWrite(str.charCodeAt(0)); // char を数値に変換して渡す
+    }
+}
+
+////////////////////////////////////////////////////////////
+// Simple notification on board for Debug!
+////////////////////////////////////////////////////////////
+function setupLed(){
+    k.pinMode(k.LED2, k.OUTPUT);
+}
+function ledOn(){
+    k.digitalWrite(k.LED2, k.HIGH);
+}
+function ledOff(){
+    k.digitalWrite(k.LED2, k.LOW);
+}
+
+////////////////////////////////////////////////////////////
+// Handlers
+////////////////////////////////////////////////////////////
+$(function(){
+
+    $(".info").text("initialize..");
+
+    $(".tap-to-send").on('click', send);
+    $(".tap-to-send").on('click', ledOff);
+
+    // serial send
+    $(".tap-to-send-char").on('click', function(ev){
+        var ascii = $(ev.target).data('key');
+        var $info = $(".info");
+        $info.empty().text('sendAscii ' + ascii);
+
+        sendAscii(ascii);
+    });
+    $(".tap-to-send-char").on('click', ledOff);
+
+    // イベント発生を通知するメッセージ
+    $(".tap").on('click', function(){
+        console.log('tapped!');
+
+        var $info = $(".info");
+        $info.empty().text('tapped!');
+        setTimeout(function(){
+            $info.empty()
+        }, 1888);
+    });
+});
---
+++ pre/sample/arduino/receive_serial_switch3/receive_serial_switch3.ino
@@ -0,0 +1,98 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+#define PIN 6
+#define PIN_DEBUG 13
+
+// Arduino Sample - Sereal Receive
+// シリアルから 文字 a を受けるたびに ボード上のLランプが光る
+char ASC_A = 'a';
+char ASC_B = 'b';
+char ASC_C = 'c';
+char ASC_Z = 'z';
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // リセット後にアップロードを許容するような マージンを持っておく
+  delay(1000);
+  
+  Serial.begin(9600);
+  pinMode(PIN_DEBUG, OUTPUT);
+
+  strip.begin();
+  strip.show();
+}
+
+void loop() { 
+  // if (Serial.available() == 2) {
+  if (Serial.available() > 0) {
+
+    // if (Serial.available() == 2) { 
+      // これはうまくいかない. 2回目のタップで一個目のタップに
+      // 該当する値が来る
+    if (Serial.available() == 1) { // これは 'a' とか 'b' とか来てるときにうまく動く      
+      char c = Serial.read();
+      
+      if (c == ASC_A) {
+        colorWipe(strip.Color(255, 0, 0), 25);
+      } else if (c == ASC_B) {
+        colorWipe(strip.Color(0, 255, 0), 25);
+      } else if (c == ASC_C) {
+        colorWipe(strip.Color(0, 0, 255), 25);
+      } else if (c == ASC_Z) {
+        colorWipe(strip.Color(1, 1, 1), 50);
+      }
+    // } else if (Serial.available() == 4) { //'#A11' などを期待
+    } else { //'#A11' などを期待
+      // このルートに入れない.. 
+      blink(2);
+      int dummy = Serial.parseInt();
+      int col_r = Serial.parseInt();
+      int col_g = Serial.parseInt();
+      int col_b = Serial.parseInt();
+      colorWipe(strip.Color(col_r, col_g, col_b), 50);
+      delay(1000);
+      colorWipe(strip.Color(0, 0, 0), 10);
+    }
+  }
+}
+
+// patterns 
+// from adafruit
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+
+// 光って消える
+void lightOnce(){
+    digitalWrite(PIN_DEBUG, HIGH);
+    delay(1300);
+    digitalWrite(PIN_DEBUG, LOW);
+    delay(1300);
+}
+
+void blink(int count){ // ...
+    for (int i=0; i<count; i++){
+      digitalWrite(PIN_DEBUG, HIGH);
+      delay(130);
+      digitalWrite(PIN_DEBUG, LOW);
+      delay(130);
+    }
+    delay(300); // space for next.
+}
+
+void nodd(){// -...
+      digitalWrite(PIN_DEBUG, HIGH);
+      delay(540);
+      digitalWrite(PIN_DEBUG, LOW);
+      delay(140);
+
+      blink(3);
+}
+
